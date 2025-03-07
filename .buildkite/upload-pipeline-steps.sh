006cc96941a0c8272c8189bbf65623f9f588e7a0
Mitch Smith <mitchpsmith1998@gmail.com>
2025-03-07 16:57:50 +1000

    Add repository example files

---
+++ .buildkite/upload-pipeline-steps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+buildkite-agent pipeline upload .buildkite/steps-1.yml
+buildkite-agent pipeline upload .buildkite/steps-2.yml
+buildkite-agent pipeline upload .buildkite/steps-3.yml
+buildkite-agent pipeline upload .buildkite/steps-4.yml
+buildkite-agent pipeline upload .buildkite/steps-5.yml
